24bd669b6570069a792a2e7a89543b7283834835
Ward van Teijlingen <w.van.teijlingen@gmail.com>
2018-08-31 22:11:25 +0200

    Added custom attributes to UnexpectedResponse.

--- lib/bunq/errors.rb
+++ lib/bunq/errors.rb
@@ -1,6 +1,19 @@
 module Bunq
-  class UnexpectedResponse < StandardError; end
+  class UnexpectedResponse < StandardError;
+    attr_reader :code
+    attr_reader :headers
+    attr_reader :body
+
+    def initialize(msg = "Unexpected response", code: nil, headers: nil, body: nil)
+      @code = code
+      @headers = headers
+      @body = body
+      super(msg)
+    end
+  end
+
   class AbsentResponseSignature < StandardError; end
   class TooManyRequestsResponse < StandardError; end
   class Timeout < StandardError; end
+  class Unauthorized < StandardError; end
 end
